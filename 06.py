6d6099e59ad5d27f1a15df7a1ca353ec59f6e058
Randy Thiemann <randy.thiemann@edt.zone>
2022-12-06 09:35:32 +0100

    don't need that data.

--- 06.py
+++ 06.py
@@ -1,18 +1,15 @@
 # Get the data from the test.
 import aoc
 orig_data = aoc.get_input(6)
-marker = "aaaa"
 
 for i in range(0, len(orig_data)-4):
     s = set(orig_data[i:i+4])
     if len(s) == 4:
-        marker = orig_data[i:i+4]
         print(f"Answer 1: {i+4}")
         break
 
 for i in range(0, len(orig_data)-14):
     s = set(orig_data[i:i+14])
     if len(s) == 14:
-        marker = orig_data[i:i+14]
         print(f"Answer 2: {i+14}")
         break
